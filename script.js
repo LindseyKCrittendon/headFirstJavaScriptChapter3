41fffc920267ff1573e82a739b2aea2f813ef536
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-15 01:34:14 -0400

    understanding returns from a function

--- script.js
+++ script.js
@@ -108,4 +108,33 @@ function barkAtTheMoon(){
 barkAtTheMoon();
 //Console shows, "Woooooooooo!"
 
+//FUNCTIONS AND RETURNS     
+// function bake(degrees){
+//     var message;
+//     if(degrees > 500){
+//         message = "I'm not a nuclear reactor!";
+//     }else if(degrees < 100){
+//         message = "I'm not a refrigerator!";
+//     } else{
+//         message = "That's a very comfortable temperature for me.";
+//         setMode("bake");
+//         setTemp(degrees);
+//     }
+//     return message;
+// }
+// var status = bake(350);
+
+function calculateArea(r){
+    var area;
+    if(r <= 0){
+        return 0;
+    }else{
+        area = Math.PI * r * r;
+        return area;
+    }
+}
+var radius = 5.2;
+var theArea = calculateArea(radius);
+console.log("The area is: " + theArea);
+
 
